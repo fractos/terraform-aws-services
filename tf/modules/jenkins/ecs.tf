1dec194e0fa40385e27d464a4c4c0e70d2b47237
Adam Christie <fractos@gmail.com>
2020-01-06 11:34:11 +0000

    Update ecs.tf

--- tf/modules/jenkins/ecs.tf
+++ tf/modules/jenkins/ecs.tf
@@ -1,5 +1,5 @@
 module "jenkins_task" {
-  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/base/"
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/tasks/base/?ref=v1.0"
 
   environment_variables = {
     "JAVA_OPTS" = "-Dhudson.footerURL=jenkins.${var.domain} -Djava.util.logging.config.file=/var/jenkins_home/log.properties"
@@ -51,7 +51,7 @@ resource "aws_ecs_task_definition" "jenkins_task" {
 }
 
 module "jenkins" {
-  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/base/web/"
+  source = "git::https://github.com/digirati-co-uk/terraform-aws-modules.git//tf/modules/services/base/web/?ref=v1.0"
 
   name       = "${var.prefix}-jenkins"
   project    = "${var.project}"
